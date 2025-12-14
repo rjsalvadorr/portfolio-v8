df2b201bdab85104fc6a7772af4d05da9671f63f
RJ Salvador <salvador.workshop@gmail.com>
2025-12-14 09:58:17 -0500

    More styling fixes

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const CURRENT_YEAR = '2025'
+export const CURRENT_YEAR_ROMAN = 'MMXXV'
+export const SITE_TITLE = 'Salvador Workshop';
+export const SITE_DESCRIPTION = `Salvador Workshop | Tech & Design [Toronto ${CURRENT_YEAR_ROMAN}]`;
